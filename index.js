9d6279471d4a22140a994b73a9561452a319e549
josephine anyango <anyangojozey@gmail.com>
2025-09-01 03:38:07 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+var newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent = 'Josephine is the champion';
+
+document.body.appendChild(newHeader);
